b6250aebc9e10f6a4ef6886c7f159bc540a387a7
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-22 20:14:48 -0300

    refactor: remove unnecessary SafeAreaView wrapper in App component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import "./global.css";
 
 import { StatusBar } from 'expo-status-bar';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import { ApolloContextProvider } from "./src/context/ApolloContext";
 
 import Routes from './src/routes';
@@ -9,12 +9,10 @@ import Routes from './src/routes';
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: '#111827' }}>
-        <StatusBar style="light" translucent />
-        <ApolloContextProvider>
-          <Routes />
-        </ApolloContextProvider>
-      </SafeAreaView>
+      <StatusBar style="light" translucent />
+      <ApolloContextProvider>
+        <Routes />
+      </ApolloContextProvider>
     </SafeAreaProvider>
   );
 }
