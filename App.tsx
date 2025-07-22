f7793a8197a0ab8560245b204a468700ba641c59
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-07-22 19:53:34 -0300

    refactor: restructure App component to use SafeAreaProvider and improve layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import "./global.css";
 
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 import { ApolloContextProvider } from "./src/context/ApolloContext";
 
 import Routes from './src/routes';
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="light" translucent />
-      <ApolloContextProvider>
-        <SafeAreaView className="flex-1 bg-gray-900">
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#111827' }}>
+        <StatusBar style="light" translucent />
+        <ApolloContextProvider>
           <Routes />
-        </SafeAreaView>
-      </ApolloContextProvider>
-    </>
+        </ApolloContextProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
